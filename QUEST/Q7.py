ff47bc3f58dd68543ab255d12cce214c550d60e4
Emma1906 <Emma@glorieux.com>
2025-04-09 10:44:54 +0200

    changement

--- QUEST/Q7.py
+++ QUEST/Q7.py
@@ -1,14 +1,11 @@
 # 7 temps moyen des pits stop en fonction des écuries en 2023 ?
 from pandas import read_csv
 import pandas as pd
+import os
 
 # On va faire un groupby sur le nom de l'écurie
-chemin_fichier_ps = (
-    r"C:\Users\djoud\OneDrive\Bureau\ENSAI\1A\projet\Projet info\Projet-Info-1A"
-    r"\donnees_formule_un\pit_stops.csv"
-)
-read_csv(chemin_fichier_ps)
-df_pits_stops = read_csv(chemin_fichier_ps)
+
+df_pits_stops = pd.read_csv(os.path.join("donnees_formule_un", "pit_stops.csv"))
 
 chemin_fichier_consresults = (
     r"C:\Users\djoud\OneDrive\Bureau\ENSAI\1A\projet\Projet info\Projet-Info-1A"
