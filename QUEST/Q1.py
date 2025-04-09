1da4662e5b09927429d402c6fc0d90907b23b34c
marylouleal <marylou.leal-fernandes@eleve.ensai.fr>
2025-04-09 11:49:02 +0200

    changements

--- QUEST/Q1.py
+++ QUEST/Q1.py
@@ -1,8 +1,4 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-import seaborn as sns
 import os
 
 #1 pilotes qui ont gagné au moins 30 courses
@@ -14,20 +10,22 @@ results = pd.read_csv(os.path.join("donnees_formule_un", "results.csv"))
 def pilotes_30_victoires(results):
     # Compter le nombre de victoires par pilote
     victoires = results[results['positionOrder'] == 1].groupby('driverId').size()
-    
+
     # Filtrer les pilotes ayant gagné au moins 30 courses
     pilotes_30_victoires = victoires[victoires >= 30].index
-    
+
     return pilotes_30_victoires
 
 
 # Lire le fichier CSV avec Pandas
 drivers = pd.read_csv(os.path.join("donnees_formule_un", "drivers.csv"))
+
 #cherchons le nom des pilotes qui ont gagné au moins 30 courses
+
 
 def nom_pilotes_30_victoires(results, drivers):
     pilotes_30_victoire = pilotes_30_victoires(results)
-    
+
     # Filtrer le DataFrame des pilotes pour obtenir les noms
     pilotes_info = drivers[drivers['driverId'].isin(pilotes_30_victoire)]\
         [['forename','surname', 'driverId']]
@@ -36,8 +34,9 @@ def nom_pilotes_30_victoires(results, drivers):
     noms_pilotes_victoires = pilotes_info.apply(
         lambda row: f"{row['forename']} {row['surname']} ({row['victoires']} victoires)", axis=1
     ).tolist()
-    
+
     return noms_pilotes_victoires
 
+
 nom_30_victoires = nom_pilotes_30_victoires(results, drivers)
-print (nom_30_victoires)
+print(nom_30_victoires)
