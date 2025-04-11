f6c12b0b4ec91b26c8586038daa9b148d6aacccb
Emma1906 <Emma@glorieux.com>
2025-04-11 17:02:43 +0200

    Q1

--- QUEST/Q1.py
+++ QUEST/Q1.py
@@ -1,4 +1,4 @@
-import pandas as pd
+"""import pandas as pd
 import os
 
 # 1 pilotes qui ont gagné au moins 30 courses
@@ -42,3 +42,62 @@ def nom_pilotes_30_victoires(results, drivers):
 
 nom_30_victoires = nom_pilotes_30_victoires(results, drivers)
 print(nom_30_victoires)
+
+
+
+"""
+
+
+import csv
+import os
+
+results = []
+
+#Ouvrir le fichier
+with open(os.path.join("donnees_formule_un", "results.csv")) as fd:
+    file = csv.DictReader(fd)
+    for line in file:
+        results.append(line)
+
+
+
+
+def pilotes_30_victoires(results):
+    # Compter le nombre de victoires par pilote
+    victoires = results[results['positionOrder'] == 1].groupby('driverId').size()
+
+    # Filtrer les pilotes ayant gagné au moins 30 courses
+    pilotes_30_victoires = victoires[victoires >= 30].index
+
+    return pilotes_30_victoires
+
+
+drivers = []
+
+#Ouvrir le fichier
+with open(os.path.join("donnees_formule_un", "drivers.csv")) as fd:
+    file = csv.DictReader(fd)
+    for line in file:
+        drivers.append(line)
+
+#cherchons le nom des pilotes qui ont gagné au moins 30 courses
+
+def nom_pilotes_30_victoires(results, drivers):
+    pilotes_30_victoire = pilotes_30_victoires(results)
+
+    # Filtrer le DataFrame des pilotes pour obtenir les noms
+    pilotes_info = drivers[drivers['driverId'].isin(pilotes_30_victoire)]\
+        [['forename','surname', 'driverId']]
+    victoires = results[results['positionOrder'] == 1].groupby('driverId').size()
+    pilotes_info['victoires'] = pilotes_info['driverId'].map(victoires)
+    noms_pilotes_victoires = pilotes_info.apply(
+        lambda row: f"{row['forename']} {row['surname']} ({row['victoires']} victoires)", axis=1
+    ).tolist()
+
+    return noms_pilotes_victoires
+
+
+nom_30_victoires = nom_pilotes_30_victoires(results, drivers)
+print(nom_30_victoires)
+
+
