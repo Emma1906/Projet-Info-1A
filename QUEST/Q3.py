327d74efd882c6ed923a93ad626a0cf36fe0c071
Emma1906 <Emma@glorieux.com>
2025-04-11 14:54:00 +0200

    Emma

--- QUEST/Q3.py
+++ QUEST/Q3.py
@@ -10,13 +10,31 @@
 import pandas as pd
 import os
 
+#importer les tables 
 constructors = pd.read_csv(os.path.join("donnees_formule_un", "constructors.csv"))
 constructors.columns = constructors.columns.str.strip()
-constructors_standings = pd.read_csv(os.path.join("donnees_formule_un", "constructors_standings.csv"))
+constructors_standings = pd.read_csv(os.path.join("donnees_formule_un", "constructor_standings.csv"))
 constructors_standings.columns = constructors_standings.columns.str.strip()
-print(constructors_standings.columns)
-print(constructors.columns)
 races = pd.read_csv(os.path.join("donnees_formule_un", "races.csv"))
 races.columns = races.columns.str.strip()
 
+#fusion des tables
+fusion = pd.merge(constructors_standings, races, on='raceId')
+fusion = fusion[['raceId', 'constructorId', 'points', 'position', 'year']]
+fusion_now = pd.merge(fusion, constructors, on='constructorId')
+print(fusion_now.columns)
+
+
+
+# Trouver le dernier raceId (la dernière course) pour chaque année
+dernieres_courses = fusion_now.groupby('year')['raceId'].max().reset_index()
+
+# Filtrer pour ne garder que les lignes correspondant à ces dernières courses
+fusion_finale = pd.merge(fusion_now, dernieres_courses, on=['year', 'raceId'])
+
+fusion_finale = fusion_finale[fusion_finale['position'].isin([1,2,3]) ] 
+fusion_finale = fusion_finale[['year', 'constructorId', 'name', 'points', 'position']]
+fusion_finale = fusion_finale.sort_values(by=['year', 'position'])
+
+return fusion_finale
  
