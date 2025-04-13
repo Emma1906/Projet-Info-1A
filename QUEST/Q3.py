540bacc455213dd54762c8b0caa8ed6998283bbd
marylouleal <marylou.leal-fernandes@eleve.ensai.fr>
2025-04-13 22:38:04 +0200

    Q3

--- QUEST/Q3.py
+++ QUEST/Q3.py
@@ -18,8 +18,7 @@ constructors_standings.columns = constructors_standings.columns.str.strip()
 races = pd.read_csv(os.path.join("donnees_formule_un", "races.csv"))
 races.columns = races.columns.str.strip()
 
-<<<<<<< HEAD
-=======
+
 #fusion des tables
 fusion = pd.merge(constructors_standings, races, on='raceId')
 fusion = fusion[['raceId', 'constructorId', 'points', 'position', 'year']]
