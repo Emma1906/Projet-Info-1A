598e8cb370f959f5fdc56e2962c48f58a33663ec
Emma1906 <Emma@glorieux.com>
2025-04-30 10:06:11 +0200

    commit

--- problématique/Emma.py
+++ problématique/Emma.py
@@ -83,3 +83,31 @@ y_pred_proba = model.predict_proba(X_test)[:, 1]
 
 print(classification_report(y_test, y_pred))
 print(f"ROC AUC Score: {roc_auc_score(y_test, y_pred_proba):.3f}")
+
+
+
+#importer les tables
+constructors = pd.read_csv(os.path.join("donnees_formule_un", "constructors.csv"))
+constructors.columns = constructors.columns.str.strip()
+constructors_standings = pd.read_csv(os.path.join("donnees_formule_un", "constructor_standings.csv"))
+constructors_standings.columns = constructors_standings.columns.str.strip()
+races = pd.read_csv(os.path.join("donnees_formule_un", "races.csv"))
+races.columns = races.columns.str.strip()
+
+
+#fusion des tables
+fusion = pd.merge(constructors_standings, races, on='raceId')
+fusion = fusion[['raceId', 'constructorId', 'points', 'position', 'year']]
+fusion_now = pd.merge(fusion, constructors, on='constructorId')
+print(fusion_now.columns)
+
+
+
+# Trouver le dernier raceId (la dernière course) pour chaque année
+dernieres_courses = df[df["year"].isin([2023,2024)].groupby['raceId'].max().reset_index()
+
+# Filtrer pour ne garder que les lignes correspondant à ces dernières courses
+fusion_finale = pd.merge(fusion_now, dernieres_courses, on=['year', 'raceId'])
+fusion_finale = fusion_finale.sort_values(by=['year', 'position'])
+df = pd.merge(fusion_finale, df, on='constructorId')
+print(fusion_finale)
